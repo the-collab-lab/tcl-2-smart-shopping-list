f75dfd6f1e2fcc004edafe31a2c4788035b37f41
Andrew Hedges <andrew@hedges.name>
2019-11-03 18:50:44 -0800

    Add quotes around Firestore env var values

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-    apiKey: {TCL_2_API_KEY},
-    authDomain: {TCL_2_AUTH_DOMAIN},
-    databaseURL: {TCL_2_DATABASE_URL},
-    projectId: {TCL_2_PROJECT_ID},
-    storageBucket: {TCL_2_STORAGE_BUCKET},
-    messagingSenderId: {TCL_2_SENDER_ID},
-    appId: {TCL_2_APP_ID},
+    apiKey: "{TCL_2_API_KEY}",
+    authDomain: "{TCL_2_AUTH_DOMAIN}",
+    databaseURL: "{TCL_2_DATABASE_URL}",
+    projectId: "{TCL_2_PROJECT_ID}",
+    storageBucket: "{TCL_2_STORAGE_BUCKET}",
+    messagingSenderId: "{TCL_2_SENDER_ID}",
+    appId: "{TCL_2_APP_ID}",
 };
 
 let fb = firebase.initializeApp(config);
