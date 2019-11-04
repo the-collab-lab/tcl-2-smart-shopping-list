df570b1a300c9dd109381530d527e92135a25dc8
Andrew Hedges <andrew@hedges.name>
2019-11-03 19:40:56 -0800

    Should probably have read the docs before naming these variables :facepalm:

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var config = {
-    apiKey: "{process.env.TCL_2_API_KEY}",
-    authDomain: "{process.env.TCL_2_AUTH_DOMAIN}",
-    databaseURL: "{process.env.TCL_2_DATABASE_URL}",
-    projectId: "{process.env.TCL_2_PROJECT_ID}",
-    storageBucket: "{process.env.TCL_2_STORAGE_BUCKET}",
-    messagingSenderId: "{process.env.TCL_2_SENDER_ID}",
-    appId: "{process.env.TCL_2_APP_ID}",
+    apiKey: "{process.env.REACT_APP_TCL_2_API_KEY}",
+    authDomain: "{process.env.REACT_APP_TCL_2_AUTH_DOMAIN}",
+    databaseURL: "{process.env.REACT_APP_TCL_2_DATABASE_URL}",
+    projectId: "{process.env.REACT_APP_TCL_2_PROJECT_ID}",
+    storageBucket: "{process.env.REACT_APP_TCL_2_STORAGE_BUCKET}",
+    messagingSenderId: "{process.env.REACT_APP_TCL_2_SENDER_ID}",
+    appId: "{process.env.REACT_APP_TCL_2_APP_ID}",
 };
 
 let fb = firebase.initializeApp(config);
