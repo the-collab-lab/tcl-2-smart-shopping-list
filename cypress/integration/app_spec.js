13b7199a7fefa271746152739765863d1e6f30d6
Ethan Bundy <gunderodd@hotmail.com>
2019-11-14 19:03:08 -0800

    Configure link to home screen and applicable test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,5 +1,8 @@
 describe('Week 1 Tests', function() {
   it('confirms the testing suite is configured', function() {
     expect(true).to.equal(true)
-	})
+  })
+  it('test to see if the home page loads', function() {
+    cy.visit('/')
+  })
 })
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>git
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Link to="/">Home</Link>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function Home() {
+  return (
+    <h1>congrats this is the home page</h1>
+  )
 }
 
 export default App;
