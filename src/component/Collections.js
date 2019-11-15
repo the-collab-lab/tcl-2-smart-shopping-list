2105ad44d9c71449cd95580f5a27304b3228cd21
michelle <moyer.mj@gmail.com>
2019-11-15 17:06:09 -0500

    created seperate component for fetching data from db

--- src/component/Collections.js
+++ src/component/Collections.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
 import { FirestoreCollection } from 'react-firestore';
 
 const Collections = (props) => {
-    return (
-      <FirestoreCollection
-          path={'users'}
-          render={(isLoading, data) => {
-            console.log(data, 'here')
+  return (
+    <FirestoreCollection
+          path="cities"
+          render={({ isLoading, data }) => {
+            return isLoading ? (
+              <div>hello two</div>
+            ) : (
+              <div>
+                <h1>Stories</h1>
+                <ul>
+                  {data.map(story => (
+                   console.log(story, 'here')
+                  ))}
+                </ul>
+              </div>
+            );
           }}
-        />
-    )
+/>
+  )
   
 }
 
