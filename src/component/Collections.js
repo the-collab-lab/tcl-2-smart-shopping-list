23abda1ca83e916fa1f7504e04c0301cf3ca002e
michelle <moyer.mj@gmail.com>
2019-11-16 15:02:42 -0500

    listed out details of collection

--- src/component/Collections.js
+++ src/component/Collections.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { FirestoreCollection } from 'react-firestore';
 
+// if i put in the wrong path it still loads the 2nd option on my tereniary operator 
+
 const Collections = (props) => {
   return (
+    //fetches specific collection, refreshes every time database chages
     <FirestoreCollection
+    //name of collection you want to collect, with filter you can narrow this down to specific document
           path="cities"
+          // optional more parameters (sort, limit, filter)
           render={({ isLoading, data }) => {
+            // if data is taking too long you can redirect or alert user it's still loading (i think)
             return isLoading ? (
-              <div>hello two</div>
+              <div>still loading</div>
             ) : (
               <div>
-                <h1>Stories</h1>
+                <h1>Collections</h1>
+                <h2>Cities</h2>
                 <ul>
-                  {data.map(story => (
-                   console.log(story, 'here')
+                  {data.map(city => (
+                    <li key={city.id}>
+                      {city.id}
+                      {city.pop}
+                    </li>
                   ))}
                 </ul>
               </div>
             );
           }}
 />
+
+          
   )
   
 }
