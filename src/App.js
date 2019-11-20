2ee453dc2b5d55395164449b72f5020fc92c0541
Ethan Bundy <gunderodd@hotmail.com>
2019-11-20 12:38:11 -0800

    Add checkToken function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import "./App.css";
 import RouterComponent from "./RouterComponent";
 import GetToken from "./component/GetToken";
 
+
+function checkToken() {
+  const checkToken = localStorage.getItem('uniqueToken')
+  // console.log(checkToken)
+
+  if (checkToken === "") {
+    console.log("no token found")
+  } else {
+    console.log("there is a token there")
+  }
+}
+
+checkToken()
+
 // For now, the App simply returns our RouterComponent, which holds all the links
 // and all of the components for each of those links.
 function App() {
-  const [gotToken, setGotToken] = useState(true);
+  const [gotToken, setGotToken] = useState(false);
 
   if (gotToken === false) {
     return <GetToken />;
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,52 @@
 import React from 'react';
+// import the script to make the tokens, which i stuck in the components folder
+import GetToken from './token'
+// attempting to make the button link as well...
+import FetchItems from './FetchItems';
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
+
+// a function to run when the button is clicked to make a token and stick it in storage
+function generateToken() {
+	const newToken = GetToken();
+	localStorage.setItem('newToken', newToken)
+	console.log(newToken)
+	checkToken(newToken)
+
+}
+
+//a function to check if the token in storage matches a list, and trying to link to a new
+// page if it does match...very sketchy
+
+function checkToken(newToken) {
+	// render () {
+	// 	if (newToken === localStorage.getItem('newToken')) {
+
+	// 	}
+	// }
+	if (newToken === localStorage.getItem('newToken')) {
+		return (
+			<div>
+				<Router>
+					<Route>
+						<FetchItems />
+					</Route>
+				</Router>
+			</div>
+		)
+	} else {
+		console.log('uh oh')
+	}
+}
 
 function Home() {
 	return (
-		<h1>congrats this is the home page</h1>
+		<div>
+			<h1>congrats this is the home page</h1>
+			{/* button for the user to make their token, which we will probably
+			just call "start a shopping list" or something in the final app */}
+			<button onClick={generateToken}>Token</button>
+
+		</div>
 	)
 }
 
