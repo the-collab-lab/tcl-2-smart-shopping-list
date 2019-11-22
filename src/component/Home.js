3eb6ce33d9cd90b25e324b12806a5f2917f775af
michelle <moyer.mj@gmail.com>
2019-11-21 21:51:57 -0500

    add token input field to home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import JoinList from './JoinList';
 
 function Home() {
   return (
     <div>
       <h1>congrats this is the home page</h1>
+      <JoinList />
     </div>
   );
 }
---
+++ src/component/JoinList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JoinList = props => {
+  return (
+    <input
+    type="text"
+    id="uniqueToken"
+    className="inputField"
+  />  )
+}
+
+export default JoinList;
