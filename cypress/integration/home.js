8168c644f818b39952ed7e82e9d05dc13d3c2f6b
mindyzwan <mindyzwan@gmail.com>
2019-11-22 22:53:07 -0800

    Fix test to check alert

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -9,22 +9,11 @@ describe("Join an existing list", function() {
 	});
 	
 	it("Alert pops up if token does not exist", function () {
-		// let alerted = '';
-		// cy.on('window:alert', msg => alerted = msg);
-		// cy.visit("/");
-		// cy.get(".inputField").type("token5678");
-		// cy.get(".joinListButton").click()
-		// 	.then(() => {
-		// 		expect(alerted).to.eq("Enter a valid share code and try again.");
-		// 	})
-
-		const stub = cy.stub()  
-		cy.on ('window:alert', stub)
 		cy.visit("/");
 		cy.get(".inputField").type("token5678");
-		cy.get(".joinListButton").click()
-    .then(() => {
-			expect(stub.getCall(0)).to.be.calledWith('Enter a valid share code and try again.')      
-    })
+		cy.get(".joinListButton").click();
+		cy.on('window:alert', str => {
+			expect(str).to.equal("Enter a valid share code and try again.")
+		});
 	})
 });
