564c43c77a5811948b3ec0a4cf11925cd28b6e04
Ethan Bundy <gunderodd@hotmail.com>
2019-11-23 11:26:58 -0600

    Fix most of older tests so that they pass

--- cypress/integration/add_item_to_list.js
+++ cypress/integration/add_item_to_list.js
@@ -1,6 +1,6 @@
 describe("Add Item To List", function() {
   it("Checks that entered items are added to list", function() {
-    cy.visit("/add");
+    cy.visit("/add").get('.tokenButton').click();
     cy.get(".inputField").type("New Cypress Item");
 
     cy.get(".addItemButton").click();
