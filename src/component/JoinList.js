f9d81f3b7279c6193192afc9aff134cb0444d06b
michelle <moyer.mj@gmail.com>
2019-11-24 11:24:14 -0500

    added comments to code to explinations and small change on CheckToken function

--- src/component/JoinList.js
+++ src/component/JoinList.js
@@ -12,29 +12,31 @@ const JoinList = ({ firestore }) => {
     }
   }
 
+  // checks if token exists and if it does, sends to that specific tokens list page, and adds token to local storage
+  //if token does ot exist, alert pops up telling you that
   const checkTokenExists = token => {
     firestore
       .collection('lists')
       .doc(token)
-      .onSnapshot(snapshot => {
-        if (snapshot.exists) {
+      .get().then(doc => {
+        if (doc.exists) {
           addToLS(token)
           setRedirect(true)
         } else {
           alert("Enter a valid share code and try again.");
         }
       })
-   
   }
 
+  // event listner watching the value of the form change and setting it to token state
   const handleChange = event => {
     setToken(event.target.value);
   }
-
+// add token to local storage
   const addToLS = token => {
     localStorage.setItem("uniqueToken", token);
   }
-
+// handles submitting form event then fires the checkToken function and sets the state token back to  an empty string 
   const handleSubmit = event => {
     event.preventDefault();
     checkTokenExists(token);
