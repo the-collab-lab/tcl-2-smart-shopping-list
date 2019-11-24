48331eba479287aa3d2efa7b215a462b48e21ab2
Ethan Bundy <gunderodd@hotmail.com>
2019-11-24 16:23:46 -0600

    Delete comments, small corrections based on review comments

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import GetToken from "./component/GetToken";
 // For now, the App simply returns our RouterComponent, which holds all the links
 // and all of the components for each of those links.
 function App() {
-  // making 
   const checkToken = localStorage.getItem('uniqueToken')
-  // console.log(checkToken)
 
   if (checkToken === null) {
     return <GetToken />;
