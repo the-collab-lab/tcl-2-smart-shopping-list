dea5e50b710a9aeb2b40d4c18a9f0b225116d3b7
Claudia Estrada <cgestrada518@gmail.com>
2019-11-25 11:38:41 -0800

    attempt to redirect to list

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GetToken from "./GetToken";
+
+function Home() {
+  return (
+    <div>
+      <h1>New Shopping List</h1>
+      <GetToken />
+      <p>
+        <a href="/join">Join Existing List</a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
