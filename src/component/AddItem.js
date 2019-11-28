39656f2cb9cafd72e91d9ec4ee0711c2d7c58f95
mindyzwan <mindyzwan@gmail.com>
2019-11-28 00:25:31 -0800

    Add frequency to database with item

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -4,13 +4,13 @@ import '../App.css';
 
 const AddItem = ({ firestore }) => {
   const [name, setName] = useState("");
-  // const [frequency, setFrequency] = useState("");
+  const [frequency, setFrequency] = useState("");
 
   //   Write item to Firebase setting uniqueToken as document name
 
   const uniqueToken = localStorage.getItem("uniqueToken");
 
-  const addItem = name => {
+  const addItem = (name, frequency) => {
     firestore
       .collection("lists")
       .doc(uniqueToken)
@@ -20,7 +20,10 @@ const AddItem = ({ firestore }) => {
       .collection("lists")
       .doc(uniqueToken)
       .collection("items")
-      .add({ name });
+      .add({ 
+        name: name,
+        frequency: frequency
+      });
   };
 
   //   Update state whenever user input changes
@@ -28,10 +31,14 @@ const AddItem = ({ firestore }) => {
     setName(event.target.value);
   };
 
+  const handleOptionChange = event => {
+    setFrequency(event.target.value);
+  }
+
   //   Trigger addItem function when "Add Item" button is clicked
   const handleSubmit = event => {
     event.preventDefault();
-    addItem(name);
+    addItem(name, frequency);
     setName("");
     // alert("item added to database!");
   };
@@ -48,19 +55,40 @@ const AddItem = ({ firestore }) => {
           className="inputField"
         />
       </label>
-      <br />
+
       <div className="frequencyButtons">
         <p>How soon are you likely to buy it again?</p>
-          <input type="radio" id="soonButton" name="frequencyButtons" value="soon" checked={ true } />
-          <label for="soonButton" id="soonButton">Soon</label>
+          <input 
+            type="radio" 
+            id="soonButton" 
+            name="frequencyButtons" 
+            value="soon" 
+            checked={frequency === "soon"} 
+            onChange={handleOptionChange}
+          />
+          <label htmlFor="soonButton" id="soonButton">Soon</label>
       
-          <input type="radio" id="prettySoonButton" name="frequencyButtons" value="pretty-soon" checked={ true } />
-          <label for="prettySoonButton" id="prettySoonButton">Pretty Soon</label>
+          <input 
+            type="radio" 
+            id="prettySoonButton" 
+            name="frequencyButtons" 
+            value="pretty-soon" 
+            checked={frequency === "pretty-soon"}
+            onChange={handleOptionChange}
+          />
+          <label htmlFor="prettySoonButton" id="prettySoonButton">Pretty Soon</label>
 
-          <input type="radio" id="notSoonButton" name="frequencyButtons" value="not-soon" checked={ true } />
-          <label for="notSoonButton" id="notSoonButton">Not Soon</label>
+          <input 
+            type="radio" 
+            id="notSoonButton" 
+            name="frequencyButtons" 
+            value="not-soon" 
+            checked={frequency === "not-soon"} 
+            onChange={handleOptionChange}
+          />
+          <label htmlFor="notSoonButton" id="notSoonButton">Not Soon</label>
       </div>
-      <br />
+
       <button onClick={handleSubmit} className="addItemButton">
         Add Item
       </button>
