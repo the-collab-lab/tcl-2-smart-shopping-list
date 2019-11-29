80cdfb8a88f27a645790801ce26de9ac45f12519
Claudia Estrada <cgestrada518@gmail.com>
2019-11-29 12:20:54 -0800

    write second test

--- cypress/integration/add_item_after_purchase.js
+++ cypress/integration/add_item_after_purchase.js
@@ -3,4 +3,12 @@ describe("Add Item After Purchase", function() {
     cy.visit("/add");
     cy.get("#soonButton").should("have.value", "soon");
   });
+
+  it("Checks that the selected radio button get focused", function() {
+    cy.visit("/add");
+    cy.get("#soonButton")
+      .click({ force: true })
+      .focused()
+      .should("have.id", "soonButton");
+  });
 });
