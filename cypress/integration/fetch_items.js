fd1423b9abcdf207765020fda9f75576adbda014
Ethan Bundy <gunderodd@hotmail.com>
2019-11-29 10:53:58 -0800

    A test

--- cypress/integration/fetch_items.js
+++ cypress/integration/fetch_items.js
@@ -1,6 +1,6 @@
 describe("Show a list", function() {
   it('Changes url to represent current view when clicked', function() {
-		cy.visit('/')
+		cy.visit('/add')
 		cy.contains('List').click()
 		cy.url().should('include', '/list')
 	})
