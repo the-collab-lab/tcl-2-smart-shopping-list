fe225f157ca28fed4a6d0ea8cdca692a290fe985
michelle jae <moyer.mj@gmail.com>
2019-11-30 17:47:20 +0000

    added where url should go after clicking back in test

--- cypress/integration/backbutton.js
+++ cypress/integration/backbutton.js
@@ -2,6 +2,7 @@ describe("Show the back button", function() {
       it('Verifies that the back button is on the add item view', function() {
         cy.visit('/add')
         cy.get('[data-cy=Back]').click()
+        cy.url().should('include', '/list')
     })
   });
   
