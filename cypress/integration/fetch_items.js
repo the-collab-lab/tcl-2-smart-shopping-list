34f3b1ca030fcb29c34271e75a3276af2fed4465
michelle jae <moyer.mj@gmail.com>
2019-12-03 14:58:21 -0500

    working on normalization test

--- cypress/integration/fetch_items.js
+++ cypress/integration/fetch_items.js
@@ -1,16 +1,21 @@
 describe("Show a list", function() {
-  it("Changes url to represent current view when clicked", function() {
-    cy.visit("/list");
-    cy.contains("List").click();
-    cy.url().should("include", "/list");
-  });
-  //   it("Checks that new item has lowercase classname", function() {
-  //     cy.visit("/add");
-  //     cy.get(".inputField").type("Strawberryaiquiri");
-  //     // cy.get("#soonButton").click({ force: true });
-  //     cy.get(".addItemButton").click();
+  it('Changes url to represent current view when clicked', function() {
+		cy.visit("/");
+		cy.get(".tokenButton").click();
+		cy.visit('/list')
+		cy.contains('List').click()
+		cy.url().should('include', '/list')
+	});
 
-  //     // cy.visit("/list");
-  //     // cy.get(".strawberry daiquiri").contains("strawberry daiquiri");
-  //   });
+    it("Checks that new item has lowercase classname", function() {
+      window.localStorage.setItem("uniqueToken", "token1234");
+      // cy.visit("/");
+      // cy.get(".tokenButton").click();
+      cy.visit("/add");
+      cy.get(".inputField").type("Toast");
+      cy.get(".addItemButton").click();
+
+      cy.visit("/list");
+      cy.contains("toast");
+    });
 });
