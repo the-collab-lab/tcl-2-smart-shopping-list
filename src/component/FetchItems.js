6c572b7e4d8699224710de982103465986ba4caf
mindyzwan <mindyzwan@gmail.com>
2019-12-04 22:46:14 -0800

    Add logic for rendering order

--- src/component/FetchItems.js
+++ src/component/FetchItems.js
@@ -10,36 +10,14 @@ import DeleteToken from "./DeleteToken";
 const FetchItems = ({ token, setToken, firestore }) => {
   const [empty, setEmpty] = useState(true);
 
-  const checkForEmpty = () => {
-    // check database and return true or false depending on empty OR set state
-
-    firestore
-      .collection('lists')
-      .doc(token)
-      .collection('items')
-      .get().then(items => {
-        if (items.empty) {
-          console.log('There are no items!');
-          setEmpty(true)
-          console.log(empty)
-
-        } else {
-          console.log('There are items')
-          setEmpty(false)
-          console.log(empty)
-        }
-      })
-  }
-
   if (!token) {
-    return <Redirect to="/" />;
-  }
-
-  if (empty) {
-    // return <button function component>
-    return (
-      <Link className="emptyAddItemButton" to="/add">Add your first Item</Link>
-    )
+    return <Redirect to="" />;
+  } else {
+    firestore
+    .collection('lists')
+    .doc(token)
+    .collection('items')
+    .get().then(items => { setEmpty(items.empty) })
   }
 
   // Token stored in user's local storage
@@ -49,34 +27,37 @@ const FetchItems = ({ token, setToken, firestore }) => {
   const concatPath = `/lists/${uniqueToken}/items`;
 
   return (
-    //fetches specific collection, refreshes every time database chages
-    <FirestoreCollection
-      //name of collection you want to collect, with filter you can narrow this down to specific document
-      path={concatPath}
-      // optional more parameters (sort, limit, filter)
-      render={({ isLoading, data }) => {
-        // if data is taking too long you can redirect or alert user it's still loading (i think)
-        return isLoading ? (
-          <div>still loading</div>
-        ) : (
-          <div>
-            <h2>Items</h2>
-
-            <ul>
-              {data.map(item => (
-                <li key={item.id}>
-                  <div className={item.name}>{item.name}</div>
-                </li>
-              ))}
-            </ul>
-            
-            <DeleteToken token={token} setToken={setToken} />
-            <Navbar />
-            { checkForEmpty() }
-          </div>
-        );
-      }}
-    />
+    <React.Fragment>
+      <FirestoreCollection
+        //name of collection you want to collect, with filter you can narrow this down to specific document
+        path={concatPath}
+        // optional more parameters (sort, limit, filter)
+        render={({ isLoading, data }) => {
+          // if data is taking too long you can redirect or alert user it's still loading (i think)
+          if (isLoading) {
+            return <div>Still Loading...</div>
+          } else if (empty) {
+            return <Link className="emptyAddItemButton" to="/add">Add your first Item</Link>
+          } else {
+            return (
+              <div>
+                <h2>Items</h2>
+                <ul>
+                  {data.map(item => (
+                    <li key={item.id}>
+                      <div className={item.name}>{item.name}</div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          }
+        }}
+        
+        />
+      <DeleteToken token={token} setToken={setToken} />
+      <Navbar />
+    </React.Fragment>
   );
 };
 
