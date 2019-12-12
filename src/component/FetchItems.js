52938b4130536771a3f1d04a745f25362810737d
mindyzwan <mindyzwan@gmail.com>
2019-12-11 20:29:50 -0800

    Add function to get data from database

--- src/component/FetchItems.js
+++ src/component/FetchItems.js
@@ -8,39 +8,61 @@ const FetchItems = ({ token, setToken, firestore }) => {
   const [empty, setEmpty] = useState(true);
   const today = new Date();
 
-
   const itemsDocRef = firestore
     .collection('lists')
     .doc(token)
-    .collection('items')
+    .collection('items');
 
   // function to change database on button click
   const handlePurchase = event => {
+    let itemId = event.target.id;
     event.preventDefault();
-    updateDatabase(event.target.id)
-  }
+    itemsDocRef
+      .doc(itemId)
+      .get()
+      .then(originalPull)
+      .then(calculateNewPurchaseValues);
+    // .then(newData => {
+    //   updateDatabase(newData)
+    // })
+  };
+
+  const originalPull = doc => {
+    let data = doc.data();
+    return {
+      latestEstimate: data.numberOfDays,
+      lastPurchase: data.dateOfPurchase.toDate(),
+      numberOfPurchases: data.numberOfPurchases,
+    };
+  };
+
+  const calculateNewPurchaseValues = data => {
+    let purchaseValues = data;
+    purchaseValues['latestInterval'] =
+      today.getTime() - purchaseValues['lastPurchase'].getTime();
+    console.log('lastpurchase', typeof purchaseValues['lastPurchase']);
+    console.log('latestEstimate', typeof purchaseValues['latestEstimate']);
+    console.log(purchaseValues);
+    return purchaseValues;
+  };
 
   // connect numberOfDays & nextPurchaseDate to estimate.js function
   // update numberOfPurchases with correct number
-  const updateDatabase = (itemId) => {
-    itemsDocRef
-      .doc(itemId)
-      .update({
-        numberOfDays: 300,
-        dateOfPurchase: today,
-        numberOfPurchases : +1,
-        nextPurchaseDate: 7000
-      })
-  }
+  const updateDatabase = (data, itemId) => {
+    itemsDocRef.doc(itemId).update({
+      numberOfDays: 300, //return value of estimate.js
+      dateOfPurchase: today,
+      numberOfPurchases: +1,
+      nextPurchaseDate: 7000, //today + numberOfDays
+    });
+  };
 
   if (!token) {
     return <Redirect to="" />;
   } else {
-    itemsDocRef
-      .get()
-      .then(items => {
-        setEmpty(items.empty);
-      });
+    itemsDocRef.get().then(items => {
+      setEmpty(items.empty);
+    });
   }
 
   // Token stored in user's local storage
@@ -74,7 +96,13 @@ const FetchItems = ({ token, setToken, firestore }) => {
                     <li key={item.id}>
                       <div className={item.name}>
                         {item.name}
-                        <button onClick={handlePurchase} className='button-link' id={item.id}>Purchase</button>
+                        <button
+                          onClick={handlePurchase}
+                          className="button-link"
+                          id={item.id}
+                        >
+                          Purchase
+                        </button>
                       </div>
                     </li>
                   ))}
