ab70483eefe2efc1e35157de64bcdee170741b2b
michelle jae <moyer.mj@gmail.com>
2019-12-18 16:59:14 -0500

    broke out sorted lists into seperate component

--- src/component/FetchItems.js
+++ src/component/FetchItems.js
@@ -5,6 +5,7 @@ import Navbar from './Navbar';
 import DeleteToken from './DeleteToken';
 import calculateNewPurchaseValues from '../calculations';
 import dayjs from 'dayjs';
+import SortedLists from './SortedLists';
 
 const FetchItems = ({ token, setToken, firestore }) => {
   const [empty, setEmpty] = useState(true);
@@ -62,60 +63,28 @@ const FetchItems = ({ token, setToken, firestore }) => {
     }
   };
 
-  // Token stored in user's local storage
-  const uniqueToken = localStorage.getItem('uniqueToken');
-
-  // unique DB path based on token
-  const concatPath = `/lists/${uniqueToken}/items`;
-
-  return (
-    <React.Fragment>
-      <FirestoreCollection
-        //name of collection you want to collect, with filter you can narrow this down to specific document
-        path={concatPath}
-        // optional more parameters (sort, limit, filter)
-        sort="numberOfDays:asc"
-        filter={['numberOfDays', '>=', 7]}
-        render={({ isLoading, data }) => {
-          // Renders according to whether or not the list is empty
-          if (isLoading) {
-            return <div>Still Loading...</div>;
-          } else if (empty) {
-            return (
-              <Link className="button-link" id="emptyListAddItem" to="/add">
-                Add your first Item
-              </Link>
-            );
-          } else {
-            return (
-              <div>
-                <h2>Items</h2>
-                <ul>
-                  {data.map(item => (
-                    <li
-                      id={item.id}
-                      key={item.id}
-                      className={calculateIfPurchased(item)}
-                    >
-                      <div
-                        className={item.name}
-                        onClick={handlePurchase}
-                        id={item.id}
-                      >
-                        {item.name}
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            );
-          }
-        }}
-      />
-      <DeleteToken token={token} setToken={setToken} />
-      <Navbar />
-    </React.Fragment>
-  );
+  if (empty) {
+    return (
+      <React.Fragment>
+        <Link className="button-link" id="emptyListAddItem" to="/add">
+          Add your first Item
+        </Link>
+        <DeleteToken token={token} setToken={setToken} />
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <SortedLists
+          token={token}
+          handlePurchase={handlePurchase}
+          calculateIfPurchased={calculateIfPurchased}
+        />
+        <Navbar />
+        <DeleteToken token={token} setToken={setToken} />
+      </React.Fragment>
+    );
+  }
 };
 
 export default withFirestore(FetchItems);
---
+++ src/component/SortedLists.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import { FirestoreCollection, withFirestore } from 'react-firestore';
+import Navbar from './Navbar';
+import DeleteToken from './DeleteToken';
+import calculateNewPurchaseValues from '../calculations';
+import dayjs from 'dayjs';
+
+const SortedList = ({ token, handlePurchase, calculateIfPurchased }) => {
+  const concatPath = `/lists/${token}/items`;
+
+  return (
+    <React.Fragment>
+      {/* // items that need to buy soon (fewer than seven days) */}
+      <FirestoreCollection
+        path={concatPath}
+        sort="numberOfDays:asc"
+        filter={['numberOfDays', '<', 7]}
+        render={({ isLoading, data }) => {
+          if (isLoading) {
+            return <div>Still Loading...</div>;
+          } else {
+            return (
+              <div>
+                <h2>Items</h2>
+                <ul>
+                  {data.map(item => (
+                    <li
+                      id={item.id}
+                      key={item.id}
+                      className={calculateIfPurchased(item)}
+                    >
+                      <div
+                        className={item.name}
+                        onClick={handlePurchase}
+                        id={item.id}
+                      >
+                        {item.name}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          }
+        }}
+      />
+      {/* // items that need to buy kind of soon (between 7 and 30 days, inclusive)  */}
+      <FirestoreCollection
+        path={concatPath}
+        sort="numberOfDays:asc"
+        filter={['numberOfDays', '<', 7]}
+        render={({ isLoading, data }) => {
+          if (isLoading) {
+            return <div>Still Loading...</div>;
+          } else {
+            return (
+              <div>
+                <h2>Items</h2>
+                <ul>
+                  {data.map(item => (
+                    <li
+                      id={item.id}
+                      key={item.id}
+                      className={calculateIfPurchased(item)}
+                    >
+                      <div
+                        className={item.name}
+                        onClick={handlePurchase}
+                        id={item.id}
+                      >
+                        {item.name}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          }
+        }}
+      />
+      {/* items that need to buy not soon (greater than 30 days)  */}
+      <FirestoreCollection
+        path={concatPath}
+        sort="numberOfDays:asc"
+        filter={['numberOfDays', '<', 7]}
+        render={({ isLoading, data }) => {
+          if (isLoading) {
+            return <div>Still Loading...</div>;
+          } else {
+            return (
+              <div>
+                <h2>Items</h2>
+                <ul>
+                  {data.map(item => (
+                    <li
+                      id={item.id}
+                      key={item.id}
+                      className={calculateIfPurchased(item)}
+                    >
+                      <div
+                        className={item.name}
+                        onClick={handlePurchase}
+                        id={item.id}
+                      >
+                        {item.name}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          }
+        }}
+      />
+      {/* items that are inactive (inactive meaning only one purchase or purchase is out date) */}
+      <FirestoreCollection
+        path={concatPath}
+        sort="numberOfDays:asc"
+        filter={['numberOfDays', '<', 7]}
+        render={({ isLoading, data }) => {
+          if (isLoading) {
+            return <div>Still Loading...</div>;
+          } else {
+            return (
+              <div>
+                <h2>Items</h2>
+                <ul>
+                  {data.map(item => (
+                    <li
+                      id={item.id}
+                      key={item.id}
+                      className={calculateIfPurchased(item)}
+                    >
+                      <div
+                        className={item.name}
+                        onClick={handlePurchase}
+                        id={item.id}
+                      >
+                        {item.name}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          }
+        }}
+      />
+    </React.Fragment>
+  );
+};
+
+export default withFirestore(SortedList);
