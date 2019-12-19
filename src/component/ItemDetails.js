1562664f754e912630b7ff8afbf437238af85b46
Claudia Estrada <cgestrada518@gmail.com>
2019-12-18 21:01:00 -0800

    fetch items from DB to ItemDetails Comp

--- src/component/ItemDetails.js
+++ src/component/ItemDetails.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { FirestoreCollection, withFirestore } from 'react-firestore';
 
-const ItemDetails = () => {
-  return <div></div>;
+const ItemDetails = props => {
+  const uniqueToken = localStorage.getItem('uniqueToken');
+  const concatPath = `/lists/${uniqueToken}/items`;
+
+  return (
+    <FirestoreCollection
+      /* //name of collection you want to collect, with filter you can narrow this
+      down to specific document  */
+      path={concatPath}
+      render={({ isLoading, data }) => {
+        // Renders according to whether or not the list is empty
+        const item = data.find(x => x.id === 'banana');
+        if (isLoading) {
+          return <div>Still Loading...</div>;
+        } else {
+          return (
+            <main>
+              <h1>{item.name}</h1>
+              <p>{item.dateOfPurchase}</p>
+            </main>
+          );
+        }
+      }}
+    />
+  );
 };
 
-export default ItemDetails;
+export default withFirestore(ItemDetails);
