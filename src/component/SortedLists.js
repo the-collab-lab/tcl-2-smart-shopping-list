39f2e44ba99a620a61df927bbcafe72fe00930ac
michelle jae <moyer.mj@gmail.com>
2019-12-24 13:53:38 -0500

    abstracted into listcontents

--- src/component/SortedLists.js
+++ src/component/SortedLists.js
@@ -51,72 +51,27 @@ const SortedList = ({ token, handlePurchase, calculateIfPurchased }) => {
               <React.Fragment>
                 <div className="soonItems">
                   <h2 className="itemsLabel">Soon Items</h2>
-                  <ul>
-                    {filteredItems.soon.map(item => (
-                      <li id={item.id} key={item.id} className="listItem">
-                        <div
-                          className={calculateIfPurchased(item)}
-                          onClick={handlePurchase}
-                          id={item.id}
-                          aria-label="Soon item"
-                          aria-required="true"
-                        >
-                          {item.name}
-                        </div>
-                        {item.dateOfPurchase ? (
-                          <Link className="viewMore" to={'/' + item.id}>
-                            >>>
-                          </Link>
-                        ) : null}
-                      </li>
-                    ))}
-                  </ul>
+                  <ListContents
+                    data={filteredItems.soon}
+                    calculateIfPurchased={calculateIfPurchased}
+                    handlePurchase={handlePurchase}
+                  />
                 </div>
                 <div className="prettySoonItems">
                   <h2 className="itemsLabel">Pretty-Soon Items</h2>
-                  <ul>
-                    {filteredItems.prettySoon.map(item => (
-                      <li id={item.id} key={item.id} className="listItem">
-                        <div
-                          className={calculateIfPurchased(item)}
-                          onClick={handlePurchase}
-                          id={item.id}
-                          aria-label="Pretty soon item"
-                          aria-required="true"
-                        >
-                          {item.name}
-                        </div>
-                        {item.dateOfPurchase ? (
-                          <Link className="viewMore" to={'/' + item.id}>
-                            >>>
-                          </Link>
-                        ) : null}
-                      </li>
-                    ))}
-                  </ul>
+                  <ListContents
+                    data={filteredItems.prettySoon}
+                    calculateIfPurchased={calculateIfPurchased}
+                    handlePurchase={handlePurchase}
+                  />
                 </div>
                 <div className="Not-Soon Items">
                   <h2 className="itemsLabel">Not-Soon Items</h2>
-                  <ul>
-                    {filteredItems.notSoon.map(item => (
-                      <li id={item.id} key={item.id} className="listItem">
-                        <div
-                          className={calculateIfPurchased(item)}
-                          onClick={handlePurchase}
-                          id={item.id}
-                          aria-label="Not soon item"
-                          aria-required="true"
-                        >
-                          {item.name}
-                        </div>
-                        {item.dateOfPurchase ? (
-                          <Link className="viewMore" to={'/' + item.id}>
-                            >>>
-                          </Link>
-                        ) : null}
-                      </li>
-                    ))}
-                  </ul>
+                  <ListContents
+                    data={filteredItems.notSoon}
+                    calculateIfPurchased={calculateIfPurchased}
+                    handlePurchase={handlePurchase}
+                  />
                 </div>
               </React.Fragment>
             );
