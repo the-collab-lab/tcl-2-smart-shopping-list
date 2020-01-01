ff0e87847a864ee3e079f0d4b5e5d4e1f28ecbe0
Ethan Bundy <gunderodd@hotmail.com>
2020-01-01 14:51:12 -0800

    Some home page changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import GetToken from './GetToken';
 import { Link } from 'react-router-dom';
+import groceries from '../backgrounds/groceries.svg';
+
+const toggleGuide = {
+  fontSize: '50px',
+  display: 'none',
+};
 
 function Home({ token, setToken }) {
   return (
     <div className="homeWrap">
-      <h1>New Shopping List</h1>
+      <img
+        alt="illustration of groceries"
+        src={groceries}
+        id="homeIllustration"
+      ></img>
+      <h1 id="homeTitle">Welcome to __(title)__!</h1>
       <GetToken token={token} setToken={setToken} />
       <Link to="/join" className="button-link" id="join-list">
         Join Existing List
       </Link>
+      <div style={toggleGuide}>BBBBBBASDFASDF</div>
+      <div
+        onMouseOver={() => {
+          console.log('hi');
+        }}
+        className="button-link"
+        id="homeGuide"
+      >
+        How Does It Work?
+      </div>
     </div>
   );
 }
