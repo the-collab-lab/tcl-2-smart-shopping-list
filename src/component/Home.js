7d1300d1927f5402cac2d71350d3724be7bb444f
Ethan Bundy <gunderodd@hotmail.com>
2020-01-02 19:29:17 -0800

    Hover guide to app

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,11 +3,6 @@ import GetToken from './GetToken';
 import { Link } from 'react-router-dom';
 import groceries from '../backgrounds/groceries.svg';
 
-const toggleGuide = {
-  fontSize: '50px',
-  display: 'none',
-};
-
 function Home({ token, setToken }) {
   return (
     <div className="homeWrap">
@@ -21,15 +16,31 @@ function Home({ token, setToken }) {
       <Link to="/join" className="button-link" id="join-list">
         Join Existing List
       </Link>
-      <div style={toggleGuide}>BBBBBBASDFASDF</div>
-      <div
-        onMouseOver={() => {
-          console.log('hi');
-        }}
-        className="button-link"
-        id="homeGuide"
-      >
+      <div className="button-link" id="homeGuide">
         How Does It Work?
+      </div>
+      <div id="hiddenGuide">
+        <ul>
+          <li>
+            1. Start a new list, or join a friend's list with the code they
+            shared
+          </li>
+          <li>
+            2. Begin by adding items that you'll need to buy in the future
+          </li>
+          <li>
+            3. Your list will be sorted with those items you need to buy first
+            at the top, and last at the bottom
+          </li>
+          <li>
+            4. Click on an item to mark it as purchased, and let __App__ figure
+            out the next date you'll need to buy it again
+          </li>
+          <li>
+            5. Click on the icon beside each item to check out its details
+          </li>
+          <li>6. Visit the menu if you want to start over with a new list</li>
+        </ul>
       </div>
     </div>
   );
