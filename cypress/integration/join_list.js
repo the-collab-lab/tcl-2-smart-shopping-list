801d8de93ab24974cf905f0b8479e8a2d1d5b9e4
mindyzwan <mindyzwan@gmail.com>
2020-01-05 12:39:25 -0800

    Fix join_list tests

--- cypress/integration/join_list.js
+++ cypress/integration/join_list.js
@@ -2,23 +2,23 @@ describe("Join an existing list", function() {
   it("Adds token to local storage on submit", function() {
     cy.visit("/join");
 		cy.get(".inputField").type("token1234");
-		cy.get("#joinListButton").click().should(() => {
+		cy.get("#join-list-button").click().should(() => {
 			expect(localStorage.getItem("uniqueToken")).to.eq("token1234")
 		});
 	});
 
 	it("Redirects to list page on valid token submit", function() {
 		cy.visit("/");
-		cy.get("#join-list").click();
+		cy.get("#joinList").click();
 		cy.get(".inputField").type("token1234");
-		cy.get("#joinListButton").click()
+		cy.get("#join-list-button").click()
 		cy.url().should('eq', Cypress.config().baseUrl + '/list')
 	});
 	
 	it("Alert pops up if token does not exist", function () {
 		cy.visit("/join");
 		cy.get(".inputField").type("token5678");
-		cy.get("#joinListButton").click();
+		cy.get("#join-list-button").click();
 		cy.on('window:alert', str => {
 			expect(str).to.equal("Enter a valid share code and try again.")
 		});
@@ -26,7 +26,7 @@ describe("Join an existing list", function() {
 
 	it("Goes back to landing page by clicking on 'Start a new list'", function() {
 		cy.visit("/join");
-		cy.get("#newListButton").click();
+		cy.get("#start-new-list-button").click();
 		cy.url().should('eq', Cypress.config().baseUrl + '/');
 	});
 });
